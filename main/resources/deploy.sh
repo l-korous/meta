06644fb3137b75ca829bdcafca12957382945a1b
Lukas Korous <lukas.korous@gmail.com>
2019-07-01 22:32:05 +0200

    Docker

--- main/resources/deploy.sh
+++ main/resources/deploy.sh
@@ -4,13 +4,16 @@
 metaHome=$META_HOME
 [[ "${metaHome}" != */ ]] && metaHome="${metaHome}/"
 
+# Deploy DB
 ${metaHome}main/target/sql/deploy_mssql.sh
-(cd ${metaHome}main/target/js && ./deploy_nodejs.sh)
-# Must be done after Nodejs (goes to public folder)
-${metaHome}main/target/html/deploy_html.sh
 
+# Create Kafka topics, Confluent plugins / scanners
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	${metaHome}main/target/kafka/deploy_kafka.sh
 fi
+
+# Create a container with Node.js app
+${metaHome}main/target/html/deploy_html.sh
+(cd ${metaHome}main/target/js && docker build .)
 
 echo Deployment done.
